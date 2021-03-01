30aaefdc5f52d620c9d7700172c1119271d8bfb6
Pitson3 <Josiahprojects1@gmail.com>
2021-03-01 11:26:49 +0200

    Add a new route to display lead details

--- Controllers/landing.js
+++ Controllers/landing.js
@@ -18,3 +18,15 @@ exports.show_leads = function(req, res, next) {
   });
   
 }
+
+
+exports.show_lead = function(req, res, next) {
+  return models.Lead.findOne({
+    where : {
+      id : req.params.lead_id
+    }
+  }).then(lead =>{
+     res.render('lead', {lead : lead});
+  });
+  
+}

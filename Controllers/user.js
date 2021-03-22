39aa3d2c9764f7557f4187c551c1a2ab817e5416
Pitson3 <Josiahprojects1@gmail.com>
2021-03-22 11:14:10 +0200

    Implemenetd error handling and form validation for signup route

--- Controllers/user.js
+++ Controllers/user.js
@@ -4,12 +4,19 @@ const passport = require("passport");
 const myPassport = require("../passport_setup")(passport);
 let flash = require("connect-flash");
 
+const { validateUser } = require("../validators/signup");
+const { isEmpty } = require("lodash");
+
 exports.show_login = function(req, res, next){
     res.render("user/login", {formData : {}, errors : {}})
 }
 
 exports.show_signup = function(req, res, next){
     res.render("user/signup", {formData : {}, errors : {}})
+}
+
+const rerender_signup = function(errors, req, res, next){
+    res.render("user/signup", {formData : req.body, errors : errors})
 }
 
 exports.login = function(req, res, next){
@@ -26,19 +33,28 @@ const generateHash = function(password){
 }
 
 exports.signup = function(req, res, next){
-    const newUser = models.User.build({
-        email : req.body.email,
-        password: generateHash(req.body.password)
-    });
+    let errors = {};
+    return validateUser(errors, req).then(errors => {
+        if(!isEmpty(errors)){
+            rerender_signup(errors, req, res, next);
+        }else{
+                
+                const newUser = models.User.build({
+                    email : req.body.email,
+                    password: generateHash(req.body.password)
+                });
 
-    return newUser.save().then(result => {
-        passport.authenticate('local', {
-            //Authenticate and redirection
-            successRedirect : '/',
-            failureRedirect : '/signup',
-            failureFlash : true
-        })(req, res, next);
+                return newUser.save().then(result => {
+                    passport.authenticate('local', {
+                        //Authenticate and redirection
+                        successRedirect : '/',
+                        failureRedirect : '/signup',
+                        failureFlash : true
+                    })(req, res, next);
+                })            
+        }
     })
+
 }
 
 exports.logout = function(req, res, next){

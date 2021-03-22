0487846377469d21e2ab98bcdc710ec73acd5cde
Pitson3 <Josiahprojects1@gmail.com>
2021-03-22 09:48:08 +0200

    Implemented the signup route and method in the users controllers

--- Controllers/user.js
+++ Controllers/user.js
@@ -1,3 +1,9 @@
+let models = require("../models");
+let bcrypt = require("bcrypt");
+const passport = require("passport");
+const myPassport = require("../passport_setup")(passport);
+let flash = require("connect-flash");
+
 exports.show_login = function(req, res, next){
     res.render("user/login", {formData : {}, errors : {}})
 }
@@ -10,6 +16,21 @@ exports.login = function(req, res, next){
     res.render("user/signup", {formData : {}, errors : {}})
 }
 
+const generateHash = function(password){
+    return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null)
+}
+
 exports.signup = function(req, res, next){
-    res.render("user/signup", {formData : {}, errors : {}})
+    const newUser = models.User.build({
+        email : req.body.email,
+        password: generateHash(req.body.password)
+    });
+
+    return newUser.save().then(result => {
+        passport.authenticate('local', {
+            successRedirect : '/',
+            failureRedirect : '/signup',
+            failureFlash : true
+        })(req, res, next);
+    })
 }
